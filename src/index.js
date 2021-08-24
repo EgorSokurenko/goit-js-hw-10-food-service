b050d810b0c48b14a293336857cf5040b8100760
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-08-24 16:57:53 +0300

    Theme done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './sass/main.scss';
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+const bodyEl = document.querySelector('.body')
+const checkThem = document.querySelector('.theme-switch__toggle')
+
+checkThem.addEventListener('input',changeTheme)
+
+let theme = localStorage.getItem('theme');
+
+localStorage.getItem('check',false);
+checkThem.checked = localStorage.getItem('check')
+
+if(theme==='dark'){
+    bodyEl.classList.remove(Theme.LIGHT)
+    bodyEl.classList.add(Theme.DARK)
+}else if(theme==='light'){
+    bodyEl.classList.remove(Theme.DARK)
+    bodyEl.classList.add(Theme.LIGHT)
+}
+
+function changeTheme(evt){
+    localStorage.setItem('theme', 'light');
+    console.log(checkThem.checked);
+    if(evt.currentTarget.checked){
+        bodyEl.classList.remove(Theme.LIGHT)
+        bodyEl.classList.add(Theme.DARK)
+        localStorage.setItem('check', 'true'); 
+        localStorage.setItem('theme', 'dark');    
+    }else if(evt.currentTarget.checked===false){
+        bodyEl.classList.remove(Theme.DARK)
+        bodyEl.classList.add(Theme.LIGHT)
+        localStorage.setItem('theme', 'light');
+        localStorage.setItem('check', 'false'); 
+        
+    }
+}
