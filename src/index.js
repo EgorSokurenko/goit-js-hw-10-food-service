e6dcb4d477f22702cb0fb4d5756bd4d06b9dce16
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-08-31 12:59:40 +0300

    всьо

--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
 import './sass/main.scss';
 import templateFunction from './block.hbs';
-import menu from './menu.json'
+import menu from './menu.json';
 
-
-const bodyEl = document.querySelector('.body')
-const menuEl = document.querySelector('.js-menu')
-const checkThem = document.querySelector('.theme-switch__toggle')
+const bodyEl = document.querySelector('.body');
+const menuEl = document.querySelector('.js-menu');
+const checkThem = document.querySelector('.theme-switch__toggle');
 
 // menuEl.innerHTML('<AccessAlarmIcon>start</AccessAlarmIcon>')
-menuEl.insertAdjacentHTML('beforeend', templateFunction(menu))
+menuEl.insertAdjacentHTML('beforeend', templateFunction(menu));
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
-checkThem.addEventListener('change',changeTheme)
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+checkThem.addEventListener('change', changeTheme);
 
 let theme = localStorage.getItem('theme');
 
-localStorage.getItem('check',false);
-checkThem.checked = localStorage.getItem('check')
-
-if(theme==='dark'){
-    bodyEl.classList.remove(Theme.LIGHT)
-    bodyEl.classList.add(Theme.DARK)
-}else if(theme==='light'){
-    checkThem.checked = false
-    bodyEl.classList.remove(Theme.DARK)
-    bodyEl.classList.add(Theme.LIGHT)
+if (theme === 'dark') {
+  checkThem.checked = true;
+  bodyEl.classList.remove(Theme.LIGHT);
+  bodyEl.classList.add(Theme.DARK);
+} else if (theme === 'light') {
+  checkThem.checked = false;
+  bodyEl.classList.remove(Theme.DARK);
+  bodyEl.classList.add(Theme.LIGHT);
 }
 
-function changeTheme(evt){
+function changeTheme(evt) {
+  localStorage.setItem('theme', 'light');
+  console.log(checkThem.checked);
+  if (evt.currentTarget.checked) {
+    bodyEl.classList.remove(Theme.LIGHT);
+    bodyEl.classList.add(Theme.DARK);
+    localStorage.setItem('theme', 'dark');
+  } else if (evt.currentTarget.checked === false) {
+    bodyEl.classList.remove(Theme.DARK);
+    bodyEl.classList.add(Theme.LIGHT);
     localStorage.setItem('theme', 'light');
-    console.log(checkThem.checked);
-    if(evt.currentTarget.checked){
-        bodyEl.classList.remove(Theme.LIGHT)
-        bodyEl.classList.add(Theme.DARK)
-        localStorage.setItem('check', 'true'); 
-        localStorage.setItem('theme', 'dark');    
-    }else if(evt.currentTarget.checked===false){
-        bodyEl.classList.remove(Theme.DARK)
-        bodyEl.classList.add(Theme.LIGHT)
-        localStorage.setItem('theme', 'light');
-        localStorage.setItem('check', 'false'); 
-        
-    }
+  }
 }
